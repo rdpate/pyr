4e619cf413b2abc5507f6ac6f42a4482569cc354
Roger Pate <roger@qxxy.com>
2017-06-12 19:50:43 -0400

    add history file, help() to console

--- py3/pyr/_bootstrap.py
+++ py3/pyr/_bootstrap.py
@@ -9,19 +9,31 @@ import sys
 import traceback
 
 def interact(opts, args):
-    import code, inspect
+    import code, inspect, site
     try:
-        import readline
+        site.removeduppaths()
+    except Exception:
+        pass
+    try:
+        site.sethelper()
+    except Exception:
+        pass
+    try:
+        import atexit, readline
     except ImportError:
         pass
     else:
         import rlcompleter
         readline.parse_and_bind("tab: complete")
+        history_fn = os.path.expanduser("~/.python{}_history".format(sys.version_info[0]))
+        if os.path.isfile(history_fn):
+            readline.read_history_file(history_fn)
+        atexit.register(readline.write_history_file, history_fn)
     locals = {"opts": opts, "args": args}
     console = code.InteractiveConsole(locals)
     if sys.version_info.major == 2:
         # v2 requires banner, this is more useful than a blank line:
-        x = ["Python " + sys.version.partition("\n")[0]]
+        x = ["Python " + sys.version.split(None, 1)[0]]
     else:
         x = [""]
     if "exitmsg" in inspect.getargspec(console.interact).args:
