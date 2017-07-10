d50114a691e5380a7fda5897908a0e89ae6077d6
Roger Pate <roger@qxxy.com>
2017-07-10 08:37:36 -0400

    append user-/system-site packages after --path; cleanup

--- py3/pyr/__init__.py
+++ py3/pyr/__init__.py
@@ -54,36 +54,6 @@ class HangupSignal(SignalExit):
 class TerminateSignal(SignalExit):
     pass
 
-def _get_target(target):
-    target, _, rest = target.partition(".")
-    try:
-        target = importlib.import_module(target)
-    except ImportError as e:
-        raise TargetLookupError(target)
-        sys.stderr.write("ImportError: {}\n".format(e))
-        sys.exit(70)
-    for x in rest.split(".") if rest else ():
-        try:
-            target = getattr(target, x)
-        except AttributeError as e:
-            if isinstance(target, types.ModuleType):
-                try:
-                    target = importlib.import_module("." + x, package=target.__name__)
-                except ImportError as ee:
-                    sys.stderr.write("ImportError: {}\n".format(ee))
-                    sys.exit(70)
-                else:
-                    continue
-            sys.stderr.write("AttributeError: {}\n".format(e))
-            sys.exit(70)
-    if isinstance(target, types.ModuleType):
-        try:
-            target = getattr(target, "main")
-        except AttributeError as e:
-            sys.stderr.write("AttributeError: {}\n".format(e))
-            sys.exit(70)
-    return target
-
 def pop_opts(args):
     """yield (name, value) options, then modify args in-place"""
     for n, x in enumerate(args):
@@ -120,9 +90,94 @@ def register_exit_signal(signum, exception=None):
     _exit_signals[signum] = exception
     signal.signal(signum, exit_signal)
 
+
 def _print_exception(e):
     sys.stderr.write("{} error...\n".format(sys.argv[0]))
     traceback.print_exception(type(e), e, sys.exc_info()[2].tb_next)
+
+def _append_site(types):
+    import site
+    def add_site_dir(dir):
+        if os.path.isdir(dir):
+            site.addsitedir(dir)
+    import_user_customize = False
+    user_done = sys_done = False
+    for dir_type in types:
+        if dir_type == "user":
+            if user_done:
+                continue
+            d = site.getusersitepackages()
+            if d:
+                add_site_dir(d)
+                import_user_customize = True
+        elif dir_type == "sys":
+            if sys_done:
+                continue
+            tail = "/lib/python{}.{}/site-packages".format(*sys.version_info[:2])
+            add_site_dir(sys.prefix + tail)
+            if sys.exec_prefix != sys.prefix:
+                add_site_dir(sys.exec_prefix + tail)
+            try:
+                import sitecustomize
+            except ImportError as e:
+                if e.name != "sitecustomize":
+                    raise
+        else:
+            sys.stderr.write("pyr error: unknown --site item: {!r}".format(dir_type))
+            sys.exit(70)
+    if import_user_customize:
+        try:
+            import usercustomize
+        except ImportError as e:
+            if e.name != "usercustomize":
+                raise
+
+def _bootstrap_setup(argv):
+    register_exit_signal(signal.SIGHUP, HangupSignal)
+    register_exit_signal(signal.SIGTERM, TerminateSignal)
+
+    dirs = argv.pop(0)[1:]
+    dirs = [] if not dirs else dirs.split(":")
+    for x in dirs:
+        if x not in sys.path:
+            sys.path.append(x)
+    site_dirs = argv.pop(0)
+    site_dirs = site_dirs.split(",") if site_dirs else []
+    _append_site(site_dirs)
+
+    target = _get_target(argv.pop(0))
+    args = argv[1:]
+    opts = list(pop_opts(args))
+    return target, opts, args
+
+def _get_target(target):
+    target, _, rest = target.partition(".")
+    try:
+        target = importlib.import_module(target)
+    except ImportError as e:
+        sys.stderr.write("pyr ImportError: {}\n".format(e))
+        sys.exit(70)
+    for x in rest.split(".") if rest else ():
+        try:
+            target = getattr(target, x)
+        except AttributeError as e:
+            if isinstance(target, types.ModuleType):
+                try:
+                    target = importlib.import_module("." + x, package=target.__name__)
+                except ImportError as ee:
+                    sys.stderr.write("pyr ImportError: {}\n".format(ee))
+                    sys.exit(70)
+                else:
+                    continue
+            sys.stderr.write("pyr AttributeError: {}\n".format(e))
+            sys.exit(70)
+    if isinstance(target, types.ModuleType):
+        try:
+            target = getattr(target, "main")
+        except AttributeError as e:
+            sys.stderr.write("pyr AttributeError: {}\n".format(e))
+            sys.exit(70)
+    return target
 
 def _bootstrap(argv):
     # PY2: lacks BrokenPipeError, handled in except IOError below, but this avoids NameError
@@ -135,16 +190,7 @@ def _bootstrap(argv):
     signal_tb = (argv.pop(0) == "true")
     try:
         try:
-            register_exit_signal(signal.SIGHUP, HangupSignal)
-            register_exit_signal(signal.SIGTERM, TerminateSignal)
-            dirs = argv.pop(0)[1:]
-            dirs = [] if not dirs else dirs.split(":")
-            for x in dirs:
-                if x not in sys.path:
-                    sys.path.append(x)
-            target = _get_target(argv.pop(0))
-            args = argv[1:]
-            opts = list(pop_opts(args))
+            target, opts, args = _bootstrap_setup(argv)
             sys.exit(target(opts, args))
         except KeyboardInterrupt as e:
             if signal_tb:
